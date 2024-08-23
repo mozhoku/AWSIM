96a482507efceac419831da4fcb2d5d8d60355c9
Alptuğ Cırıt <alptug@leodrive.ai>
2024-08-23 18:26:05 +0300

    add: gui manager
    Signed-off-by: Alptuğ Cırıt <alptug@leodrive.ai>

---
+++ Assets/AWSIM/Scripts/Loader/SimulationLauncher/LaunchPadGUIManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace AWSIM.Scripts.Loader.SimulationLauncher
+{
+    public class LaunchPadGUIManager : MonoBehaviour
+    {
+        // load initial gui
+        // update gui when a assetbundle is selected
+        // remember old loaded prefabs
+        //
+    }
+}
